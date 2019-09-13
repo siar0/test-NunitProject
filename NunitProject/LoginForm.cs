b18f6d97730bc7a37fd0e663e9be7aaaf997d9d4
siar0 <lukasz.malicki@pracuj.pl>
2019-09-13 21:16:47 +0200

    add LoginFormTests

---
+++ NunitProject/LoginForm.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace NunitProject
+{
+    [TestFixture]
+    public class LoginForm
+    {
+        private WebDriverWait wait;
+        private ChromeDriver driver;
+
+        [SetUp]
+        public void Start()
+        {
+            driver = new ChromeDriver();
+        }
+
+        [Test]
+        public void TestMethod()
+        {
+            driver.Url = "http://localhost/litecart/admin";
+            driver.FindElement(By.Name("username")).SendKeys("admin");
+            driver.FindElement(By.Name("password")).SendKeys("admin");
+            driver.FindElement(By.Name("login")).Click();
+        }
+
+        [TearDown]
+        public void Stop()
+        {
+            driver.Quit();
+        }
+    }
+}
